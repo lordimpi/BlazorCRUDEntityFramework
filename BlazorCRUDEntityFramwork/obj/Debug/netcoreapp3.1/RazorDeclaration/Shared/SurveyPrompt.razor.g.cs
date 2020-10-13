f581f470d511efa87ca55d98e4ccd82f40ddad08
Santiago Acuña <snt-26@hotmail.com>
2020-10-13 00:00:23 -0500

    Add razor compononent DishDetails

--- BlazorCRUDEntityFramwork/obj/Debug/netcoreapp3.1/RazorDeclaration/Shared/SurveyPrompt.razor.g.cs
+++ BlazorCRUDEntityFramwork/obj/Debug/netcoreapp3.1/RazorDeclaration/Shared/SurveyPrompt.razor.g.cs
@@ -76,6 +76,20 @@ using BlazorCRUDEntityFramwork.Shared;
 #line default
 #line hidden
 #nullable disable
+#nullable restore
+#line 10 "C:\Users\snt-2\Desktop\S2\BlazorCRUDEntityFramwork\BlazorCRUDEntityFramwork\_Imports.razor"
+using Models;
+
+#line default
+#line hidden
+#nullable disable
+#nullable restore
+#line 11 "C:\Users\snt-2\Desktop\S2\BlazorCRUDEntityFramwork\BlazorCRUDEntityFramwork\_Imports.razor"
+using Data;
+
+#line default
+#line hidden
+#nullable disable
     public partial class SurveyPrompt : Microsoft.AspNetCore.Components.ComponentBase
     {
         #pragma warning disable 1998
